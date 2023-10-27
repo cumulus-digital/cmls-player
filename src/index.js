fc68f0acdfdecb647906c988b57b13ceb6b9e2e9
Daniel Vena <deaconvena+github@gmail.com>
2023-10-27 11:55:17 -0400

    first stab at generalizing custom element (needs a lot of work!)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import { createContext, h } from 'preact';
-import { useLayoutEffect, useRef, useEffect } from 'preact/hooks';
+import { useLayoutEffect, useRef } from 'preact/hooks';
 import register from 'preact-custom-element';
+import { useComputed } from '@preact/signals';
 import { batch, Provider } from 'react-redux';
 
 import store from 'Store';
 import { playerStateActions } from 'Store/playerStateSlice';
 import { Station } from 'Store/Station';
+
+import { appSignals, AppContext } from './signals';
+import './subscriptions';
 
 import Logger from 'Utils/Logger';
 const log = new Logger('CMLS Player');
@@ -16,7 +20,6 @@ const config = fixSassJson(baseConfig);
 
 import { SDK } from './stream-sdk';
 import ListenLive from './ListenLive';
-import { appSignals, AppContext } from './signals';
 
 import(
 	/* webpackChunkName: 'outer' */
@@ -30,119 +33,214 @@ import(
 	}
 });
 
-function CmlsPlayerProvider(props) {
-	const dispatch = store.dispatch;
+window.customElements.define(
+	'cmls-player',
+	class extends HTMLElement {
+		stations;
+		configured = false;
 
-	const me = useRef(null);
-
-	/**
-	 * Parse DOM configuration
-	 */
-	useLayoutEffect(() => {
-		log.debug('Parsing markup config');
-
-		appSignals.sdk.type.value = props?.sdk || 'triton';
-
-		// runtime configurable minutes between preroll
-		let minutes_between_preroll = config.minutes_between_preroll;
-		if (props?.['minutes-between-preroll'] !== undefined) {
-			minutes_between_preroll =
-				parseInt(props?.['minutes-between-preroll']) ||
-				minutes_between_preroll;
+		parseAttributes(attributes) {
+			return Object.assign(
+				{},
+				...Array.from(attributes, ({ name, value }) => ({
+					[name]: value,
+				}))
+			);
 		}
-		dispatch(
-			playerStateActions['set/minutes_between_preroll'](
-				minutes_between_preroll !== NaN
-					? minutes_between_preroll
-					: config.minutes_between_preroll
-			)
-		);
 
-		const children = props?.children?.props?.children;
-		let hasPrimary = null;
-		let firstStation = null;
-		let newStations = {};
-		if (children) {
-			children.forEach((child, i) => {
-				if (child?.type === 'station') {
-					let {
-						mount,
-						name = '',
-						tagline = '',
-						logo,
-						vast,
-						primary = false,
-						order = i,
-					} = child.props;
-					if (!mount) {
-						return;
-					}
+		constructor() {
+			super();
+			this.onMutation = this.registerComponent.bind(this);
+		}
 
-					// a standalone attribute will be an empty string
-					primary = primary === false ? false : true;
+		connectedCallback() {
+			this.observer = new MutationObserver(this.onMutation);
+			this.observer.observe(this, { childList: true });
 
-					const station = new Station({
-						mount,
-						name,
-						tagline,
-						logo,
-						vast,
-						primary,
-						order,
+			this.appendChild(document.createElement('cmls-player-component'));
+		}
+
+		registerComponent() {
+			if (this.configured) return;
+
+			this.configured = true;
+
+			const dispatch = store.dispatch;
+
+			const props = this.parseAttributes(this.attributes);
+			const children = Array.from(this.childNodes);
+
+			log.debug('Parsing markup config', { props, children });
+
+			batch(() => {
+				appSignals.sdk.type.value = props?.sdk || 'triton';
+
+				let minutes_between_preroll = config.minutes_between_preroll;
+				if (props?.['minutes-between-preroll'] !== undefined) {
+					minutes_between_preroll =
+						parseInt(props?.['minutes-between-preroll']) ||
+						minutes_between_preroll;
+				}
+				dispatch(
+					playerStateActions['set/minutes_between_preroll'](
+						minutes_between_preroll !== NaN
+							? minutes_between_preroll
+							: config.minutes_between_preroll
+					)
+				);
+
+				if (props?.['offline-label'] !== undefined) {
+					appSignals.offline_label.value = props['offline-label'];
+				}
+
+				if (props?.['no-cue-label'] !== undefined) {
+					appSignals.show_cue_label.value = false;
+				}
+
+				if (props?.['background-color'] !== undefined) {
+					appSignals.background_color.value =
+						props['background-color'];
+				}
+				if (props?.['highlight-color'] !== undefined) {
+					appSignals.highlight_color.value = props['highlight-color'];
+				}
+				if (props?.['text-color'] !== undefined) {
+					appSignals.text_color.value = props['text-color'];
+				}
+
+				let newStations = {};
+				let hasPrimary = null;
+				let firstStation = null;
+				if (children) {
+					children.forEach((child, i) => {
+						if (child?.nodeName?.toLowerCase() !== 'station')
+							return;
+
+						const childProps = this.parseAttributes(
+							child.attributes
+						);
+						let {
+							mount,
+							name = '',
+							tagline = '',
+							logo,
+							vast,
+							primary = false,
+							order = i,
+						} = childProps;
+						if (!mount) {
+							return;
+						}
+
+						// a standalone attribute will be an empty string
+						primary = primary === false ? false : true;
+
+						const station = new Station({
+							mount,
+							name,
+							tagline,
+							logo,
+							vast,
+							primary,
+							order,
+						});
+
+						if (primary && !hasPrimary) {
+							hasPrimary = mount;
+						}
+
+						if (!firstStation) {
+							firstStation = mount;
+						}
+
+						newStations[mount] = Object.assign({}, station);
 					});
+				}
 
-					if (primary && !hasPrimary) {
-						hasPrimary = mount;
-					}
+				if (Object.keys(newStations)) {
+					log.debug('Initializing stations', newStations);
+					dispatch(playerStateActions['set/station'](newStations));
+				} else {
+					throw new Error('At least one station must be defined.');
+				}
 
-					if (!firstStation) {
-						firstStation = mount;
-					}
-
-					newStations[mount] = Object.assign({}, station);
+				if (hasPrimary) {
+					log.debug('Setting primary station', hasPrimary);
+					dispatch(
+						playerStateActions['set/station/primary'](hasPrimary)
+					);
+				} else {
+					// primary is first station
+					log.debug('Setting primary station', firstStation);
+					dispatch(
+						playerStateActions['set/station/primary'](firstStation)
+					);
 				}
 			});
+
+			//this.innerHTML = '';
+
+			SDK.init();
+
+			register(CmlsPlayerProvider, 'cmls-player-component', [], {
+				shadow: true,
+			});
 		}
-		batch(() => {
-			if (Object.keys(newStations)) {
-				log.debug('Initializing stations', newStations);
-				dispatch(playerStateActions['set/station'](newStations));
-			} else {
-				throw new Error('At least one station must be defined.');
-			}
-			if (hasPrimary) {
-				log.debug('Setting primary station', hasPrimary);
-				dispatch(playerStateActions['set/station/primary'](hasPrimary));
-			} else {
-				// primary is first station
-				log.debug('Setting primary station', firstStation);
-				dispatch(
-					playerStateActions['set/station/primary'](firstStation)
-				);
-			}
-		});
+	}
+);
 
-		SDK.init();
+function CmlsPlayerProvider(props) {
+	const me = useRef(null);
 
+	useLayoutEffect(() => {
 		/**
 		 * Clicking outside of our component closes dropdown
 		 */
-		window.addEventListener('click', (e) => {
+		const handleOutsideClick = (e) => {
 			const path = e.composedPath();
 			if (me?.current?.base && !path.includes(me.current.base)) {
 				//store.dispatch(playerStateActions['set/dropdown_open'](false));
 				appSignals.dropdown_open.value = false;
 			}
+		};
+		window.addEventListener('click', handleOutsideClick);
+		window.addEventListener('touchend', handleOutsideClick);
+
+		/**
+		 * Escape key closes dropdown
+		 */
+		window.addEventListener('keyup', (e) => {
+			const key = e.key;
+			if (key === 'Esc' || key === 'Escape') {
+				appSignals.dropdown_open.value = false;
+			}
 		});
 	}, []);
+
+	const cssVars = useComputed(() => {
+		const vars = [
+			`--background_color: ${appSignals.background_color}`,
+			`--highlight_color: ${appSignals.highlight_color}`,
+			`--text_color: ${appSignals.text_color}`,
+
+			`--button_top: ${appSignals.button_top}px`,
+			`--button_left: ${appSignals.button_left}px`,
+			`--button_height: ${appSignals.button_height}px`,
+			`--button_width: ${appSignals.button_width}px`,
+		];
+		return `:host { ${vars.join('; ')} }`;
+	});
 
 	return (
 		<Provider store={store}>
 			<AppContext.Provider value={appSignals}>
-				<style>{`:host > * { display: none }`}</style>
+				<style>
+					{`:host > * { display: none }`}
+					{cssVars}
+				</style>
 				<ListenLive ref={me} />
 			</AppContext.Provider>
 		</Provider>
 	);
 }
-register(CmlsPlayerProvider, 'cmls-player', [], { shadow: true });
+//register(CmlsPlayerProvider, 'cmls-player', [], { shadow: true });
